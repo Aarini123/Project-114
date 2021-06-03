c79a81bfb32d7afd7b25a4565291f557d9f45710
Aarini123 <77624143+Aarini123@users.noreply.github.com>
2021-06-03 14:54:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,12 +5,32 @@ function preload(){
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
-function draw(){
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+    console.log("x =" + results[0].pose.nose.x);
+    console.log("y =" + results[0].pose.nose.y);
+}
+}
 
+
+function modelLoaded(){
+    console.log("M0dEl_L0AdEd!!");
+}
+
+
+function draw(){
+image(video,0,0,300,300);
 }
 
 function take_snapshot(){
-    save("DOG_Image.png");
+    save("Dog_Image.png");
 }
+
